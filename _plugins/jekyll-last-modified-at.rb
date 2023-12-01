19c53903820d14abdc2e36224446686f2ae2593d
Cubie <64768472+Cubie87@users.noreply.github.com>
2023-12-01 13:09:09 +1030

    updated last-modified-at plugin

--- _plugins/jekyll-last-modified-at.rb
+++ _plugins/jekyll-last-modified-at.rb
@@ -1,23 +1,17 @@
 # _plugins/jekyll-last-modified-at.rb
 module Jekyll
     class LastModifiedAt < Liquid::Tag
-      def initialize(tag_name, file, tokens)
-        super
-        @file = file
-      end
-  
       def render(context)
-        site = context.registers[:site]
-        page = site.pages.find { |p| p.url == @file }
+        page = context.registers[:page]
   
         if page
-          last_modified = page.last_modified_at
-          formatted_date = last_modified.strftime(site.config['last_modified_at']['date_format'])
-          formatted_time = last_modified.strftime(site.config['last_modified_at']['time_format'])
+          last_modified = page["last_modified_at"] || page["date"] || page["published_at"] || page["created_at"] || File.mtime(page["path"])
+          formatted_date = last_modified.strftime(context.registers[:site].config['last_modified_at']['date_format'])
+          formatted_time = last_modified.strftime(context.registers[:site].config['last_modified_at']['time_format'])
   
           "#{formatted_date} #{formatted_time}"
         else
-          "File not found: #{@file}"
+          "Page not found"
         end
       end
     end
